a8549c46bd4a6186de48c347e5d0f59bca17379d
Felipe <pipecalgaro@gmail.com>
2022-10-30 11:46:55 -0300

    Add keyboard accessibility

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import styles from "../styles/select.module.css"
 
 export type SelectOption = {
@@ -26,6 +26,8 @@ export function Select({ multiple, value, onChange, options }: SelectProps) {
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const [highlightedIndex, setHighlightedIndex] = useState(0)
 
+  const containerRef = useRef<HTMLDivElement>(null)
+
   function clearOptions() {
     multiple ? onChange([]) : onChange(undefined)
   }
@@ -50,8 +52,43 @@ export function Select({ multiple, value, onChange, options }: SelectProps) {
     if (isOpen) setHighlightedIndex(0)
   }, [isOpen])
 
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.target != containerRef.current) return
+      switch (e.code) {
+        case "Enter":
+        case "Space":
+          setIsOpen(prev => !prev)
+          if (isOpen) selectOption(options[highlightedIndex])
+          break
+        case "ArrowUp":
+        case "ArrowDown": {
+          if (!isOpen) {
+            setIsOpen(true)
+            break
+          }
+
+          const newValue = highlightedIndex + (e.code === 'ArrowDown' ? 1 : -1)
+          if (newValue >= 0 && newValue < options.length) {
+            setHighlightedIndex(newValue)
+          }
+          break
+        }
+        case "Escape":
+          setIsOpen(false)
+          break
+      }
+    }
+    containerRef.current?.addEventListener('keydown', handler)
+
+    return () => {
+      containerRef.current?.removeEventListener('keydown', handler)
+    }
+  }, [isOpen, highlightedIndex, options])
+
   return (
     <div
+      ref={containerRef}
       onClick={() => setIsOpen(prev => !prev)}
       tabIndex={0}
       className={styles.container}
